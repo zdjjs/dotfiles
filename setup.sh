fe3b391725aeb05ce78250de45ac01f7ed789142
zdjjs <ishii@sencorp.co.jp>
2017-01-13 10:37:23 +0900

    auto dir

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 DOT_FILES=(.gitconfig .gitignore .vim .vimrc .zshrc .zshenv)
-
 for file in ${DOT_FILES[@]}
 do
-  if [ -a $HOME/$file ]; then
-    if [ -L $HOME/$file ]; then
-      echo "既にシンボリックリンクが存在します: $file"
-    elif [ -d $HOME/$file ]; then
-      echo "既にディレクトリが存在します: $file"
-    else
-      echo "既にファイルが存在します: $file"
-    fi
+  if [ -L $HOME/$file ]; then
+    rm $HOME/$file -f
+  elif [ -d $HOME/$file ]; then
+    rm -d $HOME/$file -fr
+  fi
+  if [ -e  $HOME/$file ]; then
+    echo "$HOME/$file 存在します"
   else
-    ln -s $HOME/repo/dotfiles/$file $HOME/$file
-    echo "シンボリックリンクを貼りました: $file"
+    ln -s $(cd $(dirname $0)/;pwd)/$file $HOME/$file
+    echo "$file リンクしました"
   fi
 done
+
+
